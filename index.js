54b7b054621a5ea6121a76435a727da2b60126e7
Daniel Zeh <dfzeh1@gmail.com>
2024-01-13 09:01:34 -0600

    working on js functionality

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('hello world')
+
+
+
+let addMovie = event => {
+
+    let inputField = document.querySelector('input')
+    let movie = document.createElement('li')
+    let movieTitle = document.createElement('span')
+    movieTitle.textContent = inputField.value
+    movie.appendChild(movieTitle)
+}
