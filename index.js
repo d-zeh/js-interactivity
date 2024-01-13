aa1a41dbf71ce5f4efae068cbfba4f5ce9f1d344
Daniel Zeh <dfzeh1@gmail.com>
2024-01-13 09:25:22 -0600

    Add button works!

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ console.log('hello world')
 
 
 let addMovie = event => {
+    event.preventDefault();
 
     let inputField = document.querySelector('input')
     let movie = document.createElement('li')
     let movieTitle = document.createElement('span')
     movieTitle.textContent = inputField.value
     movie.appendChild(movieTitle)
+
+    document.querySelector('ul').appendChild(movie)
+
 }
+
+ let form = document.querySelector('form')
+ form.addEventListener('submit',addMovie)
